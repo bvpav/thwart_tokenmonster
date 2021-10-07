99b0e73eb615e6e7616c2a5eb1288bbe81019a0c
bvpav <bvpav@tutanota.com>
2021-10-08 00:05:33 +0300

    Initial commit :D

---
+++ README.md
@@ -0,0 +1,56 @@
+# Thwart Token Monster
+
+A simple script that floods ongoing malicious token grabbing operations that use [:alien: Token Monster](https://github.com/Atom345/TokenMonster/blob/main/README.md) with fake data.
+
+## :book: Backstory
+
+One day, some guy joined a Discord server I was in and straight up just uploaded a [Token Monster](https://github.com/Atom345/TokenMonster/blob/main/README.md) token logger, with their own webhook edited at the top, perhaps hoping to snipe some tokens and system information.
+
+According to [the author of Token Monster](https://github.com/Atom345/TokenMonster/blob/main/README.md#mega-just-a-note), the script was intended for educational purposes and they _highly discourage using [it] against someone else_. Unfortunately, that doesn't prevent anybody from actually doing so.
+
+So I decided to take matters into my own hands and hack up this little script that floods the _1337 pwnz0r_'s webhook with thousands of _fake, but real-looking token and system captures_ that will hopefully make the life of the script kiddie slightly more miserable than it already is :heart:
+
+## :trophy: Features
+
+- Generates fake Discord authentication tokens _(that shouldn't be valid :pray:)_
+- Propagates fake full name, phone number, email, IP and MAC address.
+- Synthesizes somewhat-beliavable system information.
+- Attacker doesn't get any personally identifiable information because of the nature of Discord Webhooks.
+
+## :coffee: Usage instructions
+
+The script depends on `requests` and `faker`, which need to be installed.
+
+1. `pip install -m requirements.txt`
+2. Edit in the malicious webhook in `thwart_tokenmonster.py`
+3. `python thwart_tokenmonster.py`
+4. Sit back, relax and enjoy the show
+
+## :pushpin: TODO
+
+- [ ] Improve generated system information
+    - [ ] Use only existing `processor` strings
+    - [ ] Make `platform_version` yield more believable strings
+    - [ ] Generate better `hostname`s
+- [ ] Maintain compatibility with newer versions of Token Monster
+
+### :pray: Acknowledgments
+
+Token Monster was created by [AtomDev](https://github.com/Atom345).
+
+README was inspired by [Token Monster's](https://github.com/Atom345/TokenMonster/blob/main/README.md)
+
+## License
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
---
+++ thwart_tokenmonster.py
@@ -0,0 +1,106 @@
+import base64
+import json
+import random
+import string
+import time
+from pprint import pp
+
+import requests
+from faker import Faker
+
+WEBHOOK_URL = '<ENTER WEBHOOK URL HERE>'
+
+faker = Faker()
+num_requests = 0
+
+
+def random_string(population, k):
+    return ''.join(random.choices(population, k=k))
+
+
+def generate_token(user_id):
+    enc_id = base64.b64encode(user_id.encode('ascii')).decode('ascii')
+    chars = string.ascii_letters + string.digits + '-'
+    rand6 = random_string(chars, k=6)
+    rand27 = random_string(chars, k=27)
+
+    return '.'.join((enc_id, rand6, rand27))
+
+
+def generate_data():
+    color = random.randint(0, 0xffffff)
+    user = {
+        'id': random_string(string.digits, k=18),
+        'username': faker.name(),
+        'discriminator': random_string(string.digits, k=4),
+        'email': faker.ascii_free_email(),
+        'phone': faker.phone_number(),
+    }
+    pc = {
+        'architecture': 'AMD64',
+        'hostname': 'DESKTOP-' + random_string(string.ascii_uppercase + string.digits, k=7),
+        'ip-address': '127.0.0.1',
+        'mac-address': faker.mac_address(),
+        'platform': 'Windows',
+        'platform-release': '10',
+        'platform-version': '10.0.' + random_string(string.digits, k=5),
+        'processor': f'Intel64 Family 6 Model {random.randint(30, 80)} Stepping 1, GenuineIntel',
+        'public_ip': faker.ipv4(),
+    }
+    pc_user = user['username'].lower().replace(' ', '-')
+    pc_roaming = fr'C:\Users\{pc_user}\AppData\Roaming'
+    pc_local = fr'C:\Users\{pc_user}\AppData\Local'
+    token = generate_token(user['id'])
+    return {'username': 'TokenMonster', 'embeds': [
+        dict(title='Sniped a token.',
+             color=f'{color}',
+             fields=[
+                 {
+                     'name': '**Account Info**',
+                     'value': f'💳 User ID: ||{user["id"]}||\n🧔 Username: ||{user["username"] + "#" + user["discriminator"]}||\n📬 Email: ||{user["email"]}||\n☎ Phone: ||{user["phone"]}||',
+                     'inline': True
+                 },
+                 {
+                     'name': '**PC Info**',
+                     'value': f'IP: ||{pc["public_ip"]}|| \nUsername: {pc_user}\nAppData: {pc_local}\nRoaming: {pc_roaming}',
+                     'inline': True
+                 },
+                 {
+                     'name': '💰 Token',
+                     'value': f'||{token}||',
+                     'inline': False
+                 },
+                 {
+                     'name': '**PC Data Dump**',
+                     'value': f'```{json.dumps(pc)}```',
+                     'inline': False
+                 },
+             ]),
+    ]}
+
+
+def send_data(data):
+    global num_requests
+    pp(data)
+    print('-----')
+    response = requests.post(WEBHOOK_URL, headers={'Content-Type': 'application/json'}, data=json.dumps(data))
+    print(response.text)
+    if 200 <= response.status_code < 300:
+        num_requests += 1
+        # XXX: idk what [-] and [*] mean, they just look cool
+        print(f'[-] Successfully sent request #{num_requests}')
+
+
+def main():
+    try:
+        while True:
+            send_data(generate_data())
+            time.sleep(3)
+    finally:
+        # XXX: idk what [-] and [*] mean, they just look cool
+        # \r to erase the ^C in my terminal
+        print(f'\r[*] Sent {num_requests} request(s) to fraudulent webhook')
+
+
+if __name__ == '__main__':
+    main()
